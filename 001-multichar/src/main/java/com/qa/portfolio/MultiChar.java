a84736f4fb292a0c6b267a3f2f1a95291f2521d6
Bev Antwi <akwua8@hotmail.com>
2020-05-31 22:27:54 +0100

    multichar updated

--- 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
+++ 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
@@ -1,18 +1,37 @@
 package com.qa.portfolio;
 
-
 public class MultiChar {
+
+	private char[] answers;
 
 	/**
 	 * For each character in the entered string the method will triplicate
 	 * each the character and return the concatenated string.
 	 * 
 	 * @param input - any string
-	 * @return triplicates of each letter from the input
+	 
 	 */
+	
+	
+	
 	public String multiChar(String input) {
-		// TODO
-		return "";
-	}
-
+		
+		char Arr[]= new char[input.length()*3];
+		
+		int j = 0;
+		for (int i=0; i<input.length(); i++) {
+			
+			int count = 0;
+			while (count<3) {
+				Arr[j]=input.charAt(i);
+				count+=1;
+				j+=1;
+			}
+			
+			  
+			   
+	} 
+		input = String.valueOf(Arr);
+		return input;
+}
 }
