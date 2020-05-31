75af1307245ffde3e4be7319c1ea60ff25503d16
Bev Antwi <akwua8@hotmail.com>
2020-05-31 20:46:24 +0100

    fizzbuzz updated

--- 002-fizz-buzz/src/main/java/com/qa/portfolio/FizzBuzz.java
+++ 002-fizz-buzz/src/main/java/com/qa/portfolio/FizzBuzz.java
@@ -2,19 +2,35 @@ package com.qa.portfolio;
 
 public class FizzBuzz {
 
+	private static final String ArrayList = null;
+
 	/**
 	 * Gives string output of a numeric value by applying the fizzbuzz rules: <br>
 	 * <ul>
 	 * <li>If the number is divisible by 3 it will return "fizz"</li>
 	 * <li>If the number is divisible by 5 it will return "buzz"</li>
 	 * <li>If the number is divisible by 3 and 5 it will return "fizzbuzz"</li>
-	 * <li>If the number is divisible by neither 3 or 5 it will return the 
-	 * value of the the number as a string</li>
+	 * <li>If the number is divisible by neither 3 or 5 it will return the value of
+	 * the the number as a string</li>
 	 * </ul>
+	 * 
 	 * @param number
 	 * @return "fizz", "buzz", "fizzbuzz" or numeric value as a string
 	 */
 	public String fizzBuzz(int number) {
-    	return "";
+
+		if (number % 3 == 0) {
+			if (number % 5 == 0)
+				return "fizzbuzz";
+		} 
+		if (number % 3 == 0) {
+			return "fizz";
+
+		} else if (number % 5 == 0) {
+			return "buzz";
+
+		}
+		return (String.valueOf(number));
 	}
+
 }
