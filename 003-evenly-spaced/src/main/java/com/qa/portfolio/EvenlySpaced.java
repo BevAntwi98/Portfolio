73ec6a8fc05fc4de786e7b60a62d0db1296a233f
Bev Antwi <akwua8@hotmail.com>
2020-06-05 09:09:19 +0100

    evenly spaced updated with one error

--- 003-evenly-spaced/src/main/java/com/qa/portfolio/EvenlySpaced.java
+++ 003-evenly-spaced/src/main/java/com/qa/portfolio/EvenlySpaced.java
@@ -1,7 +1,10 @@
 package com.qa.portfolio;
 
-public class EvenlySpaced {
+import java.util.Arrays;
 
+import com.qa.connecting.Ims;
+
+public class EvenlySpaced {
 	/**
 	 * Returns true if the three values are evenly spaced <br>
 	 * Works with any order
@@ -11,8 +14,22 @@ public class EvenlySpaced {
 	 * @param num3 integer value
 	 * @return boolean value for if the 3 values entered are evenly spaced
 	 */
-	public boolean evenlySpaced(int num1, int num2, int num3) {
-		//TODO
+	public static boolean evenlySpaced(int num1, int num2, int num3) {
+
+		int nums[] = {num1, num2, num3};
+		
+	
+		Arrays.sort(nums);
+		System.out.println(nums[0]+" "+nums[1]+" "+nums[2]);
+		
+		if (nums[1]-nums[0]==nums[2]-nums[1]) {
+			System.out.println("true");
+			return true;
+			
+		}
+		System.out.println("false");
 		return false;
 	}
 }
+
+
